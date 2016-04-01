2f812d4b3d6f736338bbe4bd5820458185a7f852
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-04-01 13:17:18 +0200

    Add data folder

--- gettokens.r
+++ gettokens.r
@@ -1,4 +1,4 @@
 library(amcatr)
 c = amcat.connect("http://preview.amcat.nl")
 tokens = amcat.gettokens(c, 1006, 25173, module = "morphosyntactic", page_size = 10, only_cached = T)
-save(tokens, file="tmp/tokens.rda")
+save(tokens, file="data/tokens.rda")
