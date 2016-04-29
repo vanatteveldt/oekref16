8bf87e9923a2ea65137529b0ea2e4789baa0c3b0
kasperwelbers <kasperwelbers@gmail.com>
2016-04-29 14:02:06 +0200

    updated tokens with new codebook

--- R/codetokens.r
+++ R/codetokens.r
@@ -18,20 +18,31 @@ saveRDS(tokens, file='data/prepared_tokens_oekraine.rds')
 tokens = readRDS('data/prepared_tokens_oekraine.rds')
 
 ### prepare codebook
-cb = read.csv('codebooks/cb_oekraine.csv')
+cb = read.csv('codebooks/VD_04-29_ jk OekOntologie.csv')
 cb = prepareCodebook(cb)
 
 # adhoc schoonmaak
-cb$indicator = as.character(cb$indicator)
-cb$indicator[cb$code == 'positief'] = gsub('*', '* ', cb$indicator[cb$code == 'positief'], fixed=T)
-cb$indicator[cb$code == 'negatief'] = gsub('*', '* ', cb$indicator[cb$code == 'negatief'], fixed=T)
+#cb$indicator = as.character(cb$indicator)
+#cb$indicator[cb$code == 'positief'] = gsub('*', '* ', cb$indicator[cb$code == 'positief'], fixed=T)
+#cb$indicator[cb$code == 'negatief'] = gsub('*', '* ', cb$indicator[cb$code == 'negatief'], fixed=T)
+
+cb = cb[!is.na(cb$parent),]
+cb = cb[!cb$parent == 'ideaal',]
+
+## fix excel's special character booboos
+cb$indicator = gsub('+AH4-', '~', cb$indicator, fixed=T)
+cb$condition = gsub('+AH4-', '~', cb$condition, fixed=T)
+cb$indicator = gsub('+ACo-', '*', cb$indicator, fixed=T)
+cb$condition = gsub('+ACo-', '*', cb$condition, fixed=T)
+cb$indicator = gsub('+ACoAfg--', '*~', cb$indicator, fixed=T)
+cb$condition = gsub('+ACoAfg-', '*~', cb$condition, fixed=T)
+
 
 ### code tokens
 # first change colnames of tokens to work with codeTokens()
 colnames(tokens)[colnames(tokens) == 'aid'] = 'doc_id'
 colnames(tokens)[colnames(tokens) == 'id'] = 'position'
 
-cb = cb[!cb$parent == 'predicaat',]
 queries = data.frame(code = cb$code, 
                      indicator=cb$indicator, 
                      condition=cb$condition)
@@ -42,11 +53,13 @@ queries = queries[!is.na(queries$indicator),]
 doc_ids = unique(tokens$doc_id)
 batches = split(doc_ids, ceiling(seq_along(doc_ids)/100)) # batches of 10
 
+tokens$concept = ''
 for(i in 1:length(batches)){
   print(sprintf('%s / %s', i, length(batches)))
   selection = tokens$doc_id %in% batches[[i]]
   tokens$concept[selection] = codeTokens(tokens[selection,], queries, text_var = 'word', verbose = F)
 }
+print(queries$condition)
 
 saveRDS(tokens, file='data/prepared_tokens_oekraine.rds')
 
@@ -55,6 +68,3 @@ saveRDS(tokens, file='data/prepared_tokens_oekraine.rds')
 ## However, its probably more efficient to just look up indicators as grep indicator1|indicator2|etc. 
 
 ### try without sentiment
-#queries$code
-#squeries = queries[1:64,]
-#tokens$concept = codeTokens(tokens, squeries, text_var = 'word')
